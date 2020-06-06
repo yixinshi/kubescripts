d3f78b994821c29e9c034b9d2fedd8584782e1f5
Yixin Shi <yxshi@google.com>
2020-06-06 11:22:15 -0700

    Add some kubectl shortcuts

--- argorc.sh
+++ argorc.sh
@@ -81,3 +81,27 @@ argolist () {
   fi
   argo list $@ | head -n $num
 }
+
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+}
+
+alias k='kubectl '
+alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias kls='kubectl get pod -o=custom-columns=NODE:.spec.nodeName,NAME:.metadata.name,IP:.status.podIP '
